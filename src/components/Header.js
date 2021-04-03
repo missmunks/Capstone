7781172ce3c3c103e17dd0ad4165296fc87e2365
Savannah Walters <savwalts@gmail.com>
2021-04-03 12:31:43 -0700

    cleaning up spacing and commented out code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {
-    Nav
-  } from './';
+import logo from '../LOGO.png';
+import { Nav } from './';
 
 const style = {
     borderBottom: "1px solid #E7E7E7",
     textAlign: "center",
-    padding: "10px",
-    position: "fixed",
     left: "0",
     top: "0",
-    height: "15em",
+    height: "8em",
     width: "100%",
 };
 
@@ -21,15 +18,13 @@ const phantom = {
     width: '100%',
   }
 
-function Header({ children }) {
-    return (
-        <div>
-            <div style={phantom} />
-            <div style={style}>
+function Header({ children, token, setToken, user, setUser }) {
+    return <>
+            <img className="logo" src={logo}/>
+            <Nav token={token} setToken={setToken} user={user} setUser={setUser} style={phantom} style={style}>
                 { children }
-            </div>
-        </div>
-    )
+            </Nav>
+    </>
 }
 
 export default Header;

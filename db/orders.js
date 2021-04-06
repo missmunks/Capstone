a7ce18c824714f64cbedce2a91414ef4e7355020
Matthew Roda <matthewroda12@gmail.com>
2021-04-05 21:40:02 -0700

    getAllOrders and getOrderById db and api - tested

---
+++ db/orders.js
@@ -0,0 +1,84 @@
+const {client} = require('./index');
+const { getUserById } = require('./users');
+
+//----------------------------------------------------
+//can't make join work without distorting the data, or not returning orders without products.
+//this set of functions, using getAndAppendProducts as a return statement (promise.all'd when necessary) works.
+const getOrderProductsByOrder = async(orderId) => {
+	try{
+		const { rows: orderProducts } = await client.query(`
+			SELECT *
+			FROM order_products
+			WHERE "orderId"=${orderId}
+		`);
+		return orderProducts;
+	}
+	catch(error){
+		throw error;
+	}
+};
+
+const getAndAppendProducts = async (order) => {
+	try{
+		const orderProducts = await getOrderProductsByOrder(order.id);
+		order.products = await orderProducts;
+		return order;
+	}
+	catch(error){
+		throw error;
+	}
+};
+//----------------------------------------------------
+
+
+
+
+const getAllOrders = async() => {
+    try {
+    		console.log('GETTING ALL ORDERS');
+        const {rows: orders } = await client.query(`
+            SELECT *
+            FROM orders
+        `);
+        console.log('MAPPING ORDERS');
+       	return Promise.all(orders.map(order => getAndAppendProducts(order)));
+    }catch(error) {
+        throw error;
+    }
+};
+
+const getOrderById = async (id) => {
+	try{
+		const {rows: [order]} = await client.query(`
+			SELECT *
+			FROM orders
+			WHERE id=${id};
+		`);
+		return getAndAppendProducts(order);
+	}
+	catch(error){
+		throw error;
+	}
+};
+
+const getOrdersByUser = async ({id}) => {
+	try{
+		const {rows: orders} = await client.query(`
+			SELECT *
+			FROM orders
+			WHERE "userId"=${id};
+		`);
+		return getAndAppendProducts(order);
+	}
+	catch(error){
+		throw error;
+	}
+};
+
+
+
+module.exports = {
+	getAllOrders,
+	getOrderById,
+	getOrdersByUser,
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const apiRouter = require('express').Router();
 const productsRouter = require('./productsRouter.js');
 const usersRouter = require('./usersRouter.js');
-
+const ordersRouter = require('./ordersRouter.js');
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
@@ -12,5 +12,7 @@ apiRouter.get("/", (req, res, next) => {
 apiRouter.use("/products", productsRouter);
 
 apiRouter.use("/users", usersRouter);
+
+apiRouter.use("/orders", ordersRouter);
 
 module.exports = apiRouter;

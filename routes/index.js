693552cac18552b1542af4bb3d340af605b8adf5
Savannah Walters <savwalts@gmail.com>
2021-04-07 20:10:04 -0700

    updated routes.index.js with middleware. need to test it out

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,30 @@ apiRouter.get("/", (req, res, next) => {
   });
 });
 
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+  if (!auth) { 
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+    try {
+      const { username } = jwt.verify(token, JWT_SECRET);
+      if ( username ) {
+        req.user = await getUserByUsername( username );
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    });
+  }
+});
+
 apiRouter.use("/products", productsRouter);
 
 apiRouter.use("/users", usersRouter);

0ac890b6f5946fb48f4726f5a149abe19c35e81b
Matthew Roda <matthewroda12@gmail.com>
2021-04-08 23:28:09 -0700

    fixed middleware that adds user to request and added savs error handling

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET = "don't tell a soul"} = process.env
 const apiRouter = require('express').Router();
 const productsRouter = require('./productsRouter.js');
 const usersRouter = require('./usersRouter.js');
 const ordersRouter = require('./ordersRouter.js');
 const orderProductsRouter = require('./orderProductsRouter.js');
+const { getUserByUsername } = require('../db/users');
+
 
 apiRouter.use(async (req, res, next) => {
   const prefix = 'Bearer ';
@@ -42,5 +47,15 @@ apiRouter.use("/users", usersRouter);
 apiRouter.use("/orders", ordersRouter);
 
 apiRouter.use("/order_products", orderProductsRouter);
+
+apiRouter.get((req, res, next) => {
+  res.status(404).send(error)
+});
+
+apiRouter.use((error, req, res, next) => {
+  console.error(error)
+  res.status(500).send(error)
+});
+
 
 module.exports = apiRouter;

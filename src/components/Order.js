6fd3370aba67e3aea5fc72e12cf7092b2938b76e
Matthew Roda <matthewroda12@gmail.com>
2021-04-12 18:14:28 -0700

    a working component for getting a users orders that uses a component for individual orders. Need to make cart with similar functionality

---
+++ src/components/Order.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import {Link, useHistory, useParams} from 'react-router-dom';
+import { getOrderById } from '../api';
+
+
+const Order = ({order}) => {
+
+	return <div>
+	
+		<div>THIS WILL BE ONE ORDER</div>
+		<div className='single-order'>
+			<h3>Order id: {order.id}</h3>
+			<h3>Date Placed: {order.datePlaced}</h3>
+			<h3>Status: {order.status}</h3>
+			<h3>Products:</h3>
+			<ul>
+				{order.products.map(product => {
+					return <div className='order-product' key={product.id}>
+						<h4>Product ID: {product.id}</h4>
+						<h4>Price: {product.price}</h4>
+						<h4>Quantity: {product.quantity}</h4>
+					</div>
+				})}
+			</ul>
+		</div>
+	
+	</div>
+};
+
+
+
+
+
+export default Order;

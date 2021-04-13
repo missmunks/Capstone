800be62e78016a067cbff0a7577d606b8b05fd52
Savannah Walters <savwalts@gmail.com>
2021-04-12 21:32:28 -0700

    took out react-bootstrap

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Link, useHistory, useParams} from 'react-router-dom';
 import { getOrderById } from '../api';
-import {Button, Card} from 'react-bootstrap';
 
 
 const Order = ({order}) => {

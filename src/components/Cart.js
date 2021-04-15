2dfde335fd5efeec99f4c6ea1ebd8b6bc3b46744
Matthew Roda <matthewroda12@gmail.com>
2021-04-14 18:54:04 -0700

    added functional api calls to Savs products buttons

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,22 +6,7 @@ import { getCart } from '../api';
 
 //needs to call GET /orders/cart with a token in the header, and render an Order component with the returned "order"
 const Cart = ({cart, setCart, token}) => {
-	const fetchAndSetCart = async (token) => {
-		try{
-			const queriedCart = await getCart(token);
-			setCart(queriedCart);
-		}
-		catch(error){
-			console.log(error);
-		}
-	};
-	
-	useEffect(()=>{
-		// fetchAndSetCart(token);
-	} , [token]);
 
-	// when is it the correct time to set the cart? login? logout?
-	// what state variables are neccessary?
 	
 	return <Order order={cart} type={'cart'}/>
 };

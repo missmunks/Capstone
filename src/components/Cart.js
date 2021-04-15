cbd6e0f5fe6cbeffd7914223f435a8e61327ece0
Savannah Walters <savwalts@gmail.com>
2021-04-14 18:56:04 -0700

    cleaned up commented out code

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,6 @@ import {Order} from './';
 import { getCart } from '../api';
 
 
-//needs to call GET /orders/cart with a token in the header, and render an Order component with the returned "order"
 const Cart = ({cart, setCart, token}) => {
 	const fetchAndSetCart = async (token) => {
 		try{
@@ -19,24 +18,8 @@ const Cart = ({cart, setCart, token}) => {
 	useEffect(()=>{
 		// fetchAndSetCart(token);
 	} , [token]);
-
-	// when is it the correct time to set the cart? login? logout?
-	// what state variables are neccessary?
 	
 	return <Order order={cart} type={'cart'}/>
 };
 
 export default Cart;
-
-
-	// local storage: cart is an object
-	// how do you store an object within local storage?
-	// useEffect-- how do you know if you should use cart in local storage or in the database (ie: useEffect on line 19)
-	// const [token, setToken] = useState( () => {
-		// if (localStorage.getItem('token')) {
-		// 	return localStorage.getItem('token')
-		// } else {
-		// 	return ''
-		// }
-
-	// also possible to update the database instead of local

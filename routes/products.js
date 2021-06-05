623d35c26775d69c7db652b91a3f61b3128a3070
missmunks <missmunks@LAPTOP-KQ942D85.localdomain>
2021-06-04 17:24:07 -0700

    admin

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { getAllProducts } = require('../db/products');
 const productsRouter = express.Router();
+
+
 productsRouter.get('/', async(req, res, next) => {
     try {
         const allProducts = await getAllProducts()
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -24,4 +24,17 @@ productsRouter.get('/:id', async(req, res, next) => {
 	}
 });
 
+const destroyProduct = async ({ id }) => {
+    try {
+        const {rows: [product] = await client.query(`
+        DELETE FROM products
+        WHERE id=$1
+        `, [id])
+    }
+   return order
+    } catch (error) {
+        throw error
+    }
+}
+
 module.exports = productsRouter;
